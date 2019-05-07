fc2b8717f2c8e0d3a5b297d8292d102063f5f8fc
Honey Pilania <honey.pilania@socwisetechnologies.com>
2019-05-07 15:18:40 +0530

    changes in main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+
+// Print Hello World
+console.log('Hello world..!');
+
+// Trying to make object
+var user = {
+    name:"Honey Pilania",
+    age: 23,
+    email:"honey.pilania@socwisetechnologies.com"
+}
+
+console.log(user); 
+console.log(user.name);
+
+// Anonymous function
+var person = function(){
+    console.log("Hi This is Honey Pilania..!");
+}
+person();
+
+// Anonymous Function with Arguments
+var Add = function(a,b){
+    var c = a+b;
+    console.log("Addition of A and B = " +c);
+}
+
+Add(2,9);
+
